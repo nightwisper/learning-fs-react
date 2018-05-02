e6ce53c575b391c072373971775d2ef39a36b648
Lucas Silva <lucas.nascimento@gmail.com>
2018-05-01 19:19:40 -0700

    Google oauth setup

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+const express = require("express");
+require("./services/passport");
+
+const app = express();
+require("./routes/authRoutes")(app);
 
 app.listen(PORT, err => {
   if (err) {
